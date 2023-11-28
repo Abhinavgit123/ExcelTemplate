ac3b85c61029961189e48624b40432ab69d524ee
Abhinav Raman <abhinavraman1203@gmail.com>
2023-11-28 22:18:11 +0530

    Updated retrieve data api

--- demo/src/main/java/com/excelTemplate/demo/Controller/OrganizationDataController.java
+++ demo/src/main/java/com/excelTemplate/demo/Controller/OrganizationDataController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/organizations")
@@ -20,14 +21,31 @@ public class OrganizationDataController {
 
 
     @PostMapping("/{orgId}/add-data")
-    public OrganizationData addData(@PathVariable String orgId, @RequestBody Map<String, List<String>> data) {
-        return organizationDataService.addData(orgId, data);
+    public ResponseEntity<String> addData(@PathVariable String orgId, @RequestBody Map<String, List<String>> data) {
+        OrganizationData res= organizationDataService.addData(orgId, data);
+        if(res!=null){
+            String message="\"Data has been added to orgId: \" + orgId";
+            return new ResponseEntity<>(message,HttpStatus.OK);
+        }else{
+            String errorMessage = "Failed to add data to orgId: " + orgId;
+            return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{orgId}/retrieve-data")
-    public OrganizationData retrieveData(@PathVariable String orgId) {
-        return organizationDataService.retrieveData(orgId);
+    public ResponseEntity<Object> retrieveDataResponse(@PathVariable String orgId) {
+        OrganizationData data = organizationDataService.retrieveData(orgId);
+
+        if (data != null) {
+            // Data found, return a success response
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        } else {
+            // No data found, return a not found response with a custom message
+            String errorMessage = "No data found for orgId: " + orgId;
+            return new ResponseEntity<>(errorMessage, HttpStatus.OK);
+        }
     }
+
 
     @GetMapping("/{orgId}/download-excel")
     public ResponseEntity<byte[]> downloadExcel(@PathVariable String orgId) {
